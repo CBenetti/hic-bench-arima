178a9f48885bf61cee66c36a62cb4c8011665e10
Boccalatte <boccaf01@bigpurple-ln2.cm.cluster>
2025-11-06 16:47:51 -0500

    Automate new scripts attempt 2

--- pipelines/hicseq-standard/pipeline/compartments/submit_saddle_plot.sh
+++ pipelines/hicseq-standard/pipeline/compartments/submit_saddle_plot.sh
@@ -7,7 +7,7 @@
 ##usage: select two branches (one for hic and one for compartments) and modify saddle_plots.R
 ##	then chose a branch and modify the following line to list the samples
 
-branch = "results/tracks.by_sample.juicer_cool.multires/filter.by_sample.mapq_20_mindist0/align.by_sample.hicpro"
+branch = "results/compartments.by_sample.homer.res_100kb/filter.by_sample.mapq_20_mindist0/align.by_sample.hicpro"
 
 for sample in "$branch"/*; do
   sample_name=$(basename "$sample")
