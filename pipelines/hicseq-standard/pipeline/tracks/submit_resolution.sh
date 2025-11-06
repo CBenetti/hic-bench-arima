9178a782c74e8cc0e3307dbbf9fe198a24bcabfd
Boccalatte <boccaf01@bigpurple-ln2.cm.cluster>
2025-11-06 17:35:36 -0500

    Fix new script automation attempt 3

--- pipelines/hicseq-standard/pipeline/tracks/submit_resolution.sh
+++ pipelines/hicseq-standard/pipeline/tracks/submit_resolution.sh
@@ -10,7 +10,7 @@ conda activate neoloops
 ##usage: chose resolutions to calculate and modify calculate_resolution.py
 ##	then chose a branch and modify the following line
 
-branch = "results/tracks.by_sample.juicer_cool.multires/filter.by_sample.mapq_20_mindist0/align.by_sample.hicpro"
+branch="results/tracks.by_sample.juicer_cool.multires/filter.by_sample.mapq_20_mindist0/align.by_sample.hicpro"
 
 for sample in "$branch"/*; do
   sample_name=$(basename "$sample")
